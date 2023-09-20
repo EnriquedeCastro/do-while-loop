672a2bcb709457da37007e10d0a1ddbbc1b63893
EnriquedeCastro <143217608+EnriquedeCastro@users.noreply.github.com>
2023-09-20 11:09:18 -0700

    Add files via upload

---
+++ do-while-loop/whileloops.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class whileloops
+{
+    public static void main(String[] args) {
+        int i = 0;
+        int k = 11;
+        int a = 0;
+        int b = 0;
+        do { 
+            System.out.println("i is" + " " + i + " " + "but less than 15");
+            i = i + 5;
+        }
+        while (i < 15);
+        do {
+            System.out.println ("k is" + " " + k + " " + "but greater than 5");
+            k = k - 2; 
+        }
+        while (k > 5);
+        do {
+            System.out.println ("a is less than 10" + " " + "and b is less than or equal to 20");
+            a = a + 5;
+            b = b + 5;
+        }
+        while ((a < 10) && (b <= 20));
+    }
+}
+ 
